9fe2cd1168ef7e54a7e7109d065720ec172a2bd3
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2024-03-12 00:46:58 +0530

    product list logic finished

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -14,6 +14,18 @@ const ListProduct = () => {
   useEffect(()=>{
     fetchInfo();
   },[])
+
+  const remove_product  = async (id)=>{
+    await fetch('http://localhost:4000/removeproduct',{
+      method:'POST',
+      headers:{
+        Accept:'application/json',
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({id:id})
+    })
+    await fetchInfo();
+  }
   return (
     <div className='list-product'>
       <h1>All Products List</h1>
@@ -28,14 +40,16 @@ const ListProduct = () => {
       <div className="listproduct-allproducts">
         <hr />
         {allproducts.map((product,index)=>{
-            return <div key={index}className="listproduct-format-main listproduct-format">
+            return <><div key={index}className="listproduct-format-main listproduct-format">
                   <img src={product.image} alt="" className="listproduct-product-icon" />
-                  <p>{product.title}</p>
+                  <p>{product.name}</p>
                   <p>${product.old_price}</p>
                   <p>${product.new_price}</p>
                   <p>{product.category}</p>
-                  <img src={cross_icon} alt="" className='listproduct-remove-icon' />
+                  <img onClick={()=>remove_product(product.id)}src={cross_icon} alt="" className='listproduct-remove-icon' />
             </div>
+            <hr />
+            </>
         })}
       </div>
     </div>
